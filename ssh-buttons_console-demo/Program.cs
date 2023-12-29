e30dbb610f884ddeb6d3f320d63ed7437bd217b5
Filip Komárek <filip2cz>
2023-12-29 14:47:16 +0100

    EDIT: separate config and commands into two files

--- ssh-buttons_console-demo/Program.cs
+++ ssh-buttons_console-demo/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System.Diagnostics;
 using ssh_buttons_console_demo;
 
-string ver = "v0.1";
+string ver = "v0.2-devel";
 
 Config configLoader = new Config();
 
 string[] config = configLoader.LoadConfig();
 
+if (config[0] == "error")
+{
+    Console.WriteLine("Error loading config file:");
+    Console.WriteLine(config[1]);
+}
+
 Debug.WriteLine(config);
 
 Ssh ssh = new Ssh();
 
+Debug.WriteLine("Loaded config:");
 foreach (string prvek in config)
 {
     Debug.WriteLine(prvek);
@@ -25,30 +32,36 @@ Console.WriteLine();
 string input = "-1";
 
 string hostname;
-if (config[0] == "askUser")
+if (config[1] == "askUser")
 {
     Console.Write("Server hostname: ");
     hostname = Console.ReadLine();
 }
 else
 {
-    hostname = config[0];
+    hostname = config[1];
 }
 
+Debug.WriteLine($"Hostname: {hostname}");
+
 string username;
-if (config[1] == "askUser")
+if (config[2] == "askUser")
 {
     Console.Write("Username: ");
     username = Console.ReadLine();
 }
 else
 {
-    username = config[1];
+    username = config[2];
 }
+
+Debug.WriteLine($"Username: {username}");
 
 Console.Write("Password: ");
 string password = ShowStars();
 Console.WriteLine();
+
+string[] commands = configLoader.LoadCommands();
 
 string output = string.Empty;
 
@@ -59,12 +72,12 @@ while (input != "0")
 {
     bool z = true;
     int inputMaxLenght = 1;
-    int y = 2;
+    int y = 0;
     while (z)
     {
         try
         {
-            Console.WriteLine($"[{inputMaxLenght}] {config[y]}");
+            Console.WriteLine($"[{inputMaxLenght}] {commands[y]}");
             inputMaxLenght++;
             y += 2;
         }
@@ -82,7 +95,7 @@ while (input != "0")
     input = Console.ReadLine();
 
     Debug.WriteLine($"input = {input}");
-    Debug.WriteLine($"config.Length = {config.Length}");
+    Debug.WriteLine($"commands.Length = {commands.Length}");
     Debug.WriteLine($"inputMaxLenght = {inputMaxLenght}");
 
     if (input == "")
@@ -102,7 +115,7 @@ while (input != "0")
     {
         Console.Clear();
         Console.WriteLine("Running command");
-        output = ssh.Command(hostname, username, password, config[int.Parse(input)*2+1]);
+        output = ssh.Command(hostname, username, password, commands[int.Parse(input)*2-1]);
         Console.WriteLine(output);
         Console.WriteLine("------------------------");
     }
