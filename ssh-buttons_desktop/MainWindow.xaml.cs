829e99def419efd9abf8768ffb18a2b9f8ecdafa
filip2cz <filip@fkomarek.eu>
2024-01-24 14:56:32 +0100

    ADD: some grids

--- ssh-buttons_desktop/MainWindow.xaml.cs
+++ ssh-buttons_desktop/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,38 @@ namespace ssh_buttons_desktop
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Button1 clicked");
+        }
+        public void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Button2 clicked");
+        }
+        public void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Button3 clicked");
+        }
+        public void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Button4 clicked");
+        }
+        public void Button_Click5(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Button5 clicked");
+        }
+        public void Button_Click6(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Button6 clicked");
+        }
+        public void Button_Click7(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Button7 clicked");
+        }
+        public void Button_Click8(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Button8 clicked");
         }
     }
 }
